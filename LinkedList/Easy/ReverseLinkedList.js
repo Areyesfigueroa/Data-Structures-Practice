9d117d4a7c59cc742d45077a911047062fd5ec47
Aliel Reyes <alielreyes@gmail.com>
2021-02-06 11:22:06 -0800

    Reversing a linked list, still in progress.

--- LinkedList/Easy/ReverseLinkedList.js
+++ LinkedList/Easy/ReverseLinkedList.js
@@ -1,11 +1,32 @@
 const { SinglyLinkedListNode } = require('../../utils/linkedListUtils');
-
 utils = require('../../utils/linkedListUtils');
 
-const reverse = (head) => {
-    
+const reverse = (currentNode, prevNode = null) => {
+    if(currentNode === null) {
+        return;
+    } else {
+        reverse(currentNode.next, currentNode);
+
+        if(prevNode != null) {
+            currentNode.next = prevNode;
+            //1->2->3->null
+            //3->2(c), 2->3(p) 
+            //2->1(c), 1->2(p)
+            //1->null
+            console.log("Current Node: " + currentNode.data);
+            console.log("Current Node's Next: " + currentNode.next.data);
+            console.log("PrevNode: " + prevNode.data);
+            console.log("PrevNode's Next: " + prevNode.next.data);
+        } else {
+            currentNode.next = null;
+        }
+    }
 }
 
-let head = new utils.SinglyLinkedListNode(1);
+let head = new SinglyLinkedListNode(1);
+head.next = new SinglyLinkedListNode(2);
+head.next.next = new SinglyLinkedListNode(3);
+utils.printList(head);
+reverse(head);
 utils.printList(head);
 
